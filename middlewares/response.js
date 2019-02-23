241c13751eec14314c93393af2b962a60280c790
cat <2574522520@qq.com>
2019-02-23 13:28:54 +0800

    'wechat'

--- middlewares/response.js
+++ middlewares/response.js
@@ -14,10 +14,10 @@
 module.exports = async (ctx, next) => {
     ctx.error = ({ data, msg, status, error }) => {
         ctx.status= status || 400;
-        ctx.body = { code: -200, msg, data, error };
+        ctx.body = { code: -1, msg, data, error };
     }
     ctx.success = ({ data, msg }) => {
-        ctx.body = { code: 200, msg, data };
+        ctx.body = { code: 0, msg, data };
     }
     await next();
 }
