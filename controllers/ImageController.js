f781abd84bf5852b4595ef6442d8231b45a02ab9
cat <2574522520@qq.com>
2019-02-23 10:56:00 +0800

    'wechat'

--- controllers/ImageController.js
+++ controllers/ImageController.js
@@ -21,15 +21,7 @@ class ImageController extends BaseController{
         console.log('=================');
         logUtil.logDebug('888888888888');
 
-        // 上传单个文件
-        const file = ctx.request.files.file; // 获取上传文件
-        // 创建可读流
-        const reader = fs.createReadStream(file.path);
-        let filePath = path.join(__dirname, 'static/upload/') + `/${file.name}`;
-        // 创建可写流
-        const upStream = fs.createWriteStream(filePath);
-        // 可读流通过管道写入可写流
-        reader.pipe(upStream);
+
 
         return ctx.success({
             msg:'登录成功',
