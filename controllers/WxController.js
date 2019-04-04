1585a1328d35d5b93d5b6840997688e9a5acb20b
cat <2574522520@qq.com>
2019-04-04 16:39:48 +0800

    'pay'

--- controllers/WxController.js
+++ controllers/WxController.js
@@ -436,6 +436,7 @@ class WxController extends BaseController{
             console.log('4444444444444')
             //转到客服系统
             let xml = await WechatService.transfer_customer_service(wxData.ToUserName, wxData.FromUserName);
+            return xml;
             console.log(xml)
         }
     }
