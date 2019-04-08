329e1147706de645fdd63f33df1c5ff724ef7951
cat <2574522520@qq.com>
2019-04-08 09:20:25 +0800

    'wechat'

--- fs.js
+++ fs.js
@@ -5,14 +5,6 @@ const download = require('download');
 
 (async ()=>{
     let media_path = 'https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=gQHO8jwAAAAAAAAAAS5odHRwOi8vd2VpeGluLnFxLmNvbS9xLzAyMThTVkZoMWtlM0QxMDAwMDAwN3IAAgSFn6lcAwQAAAAA';
-//let data = fs.readFileSync(media_path);
-//let data = fs.createReadStream(media_path);
-
-// let data = request.post(media_path, {
-//     form: {
-//         param:"param"
-//     }
-// }).pipe(fs.createWriteStream('./static/service/a.jpg'))
 
     console.log("00000000000");
     let data = await download(media_path).pipe(fs.createWriteStream('./static/service/a.png'));
